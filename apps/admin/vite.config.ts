ec4d0a7d6c23dcc55ab8e07db543495244c8129a
Aksar La'ode <101698503+laodeaksar@users.noreply.github.com>
2026-04-30 00:03:44 +0000

    web init

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -6,14 +6,18 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+import viteTsConfigPaths from "vite-tsconfig-paths"
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
-    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    nitro(),
     tailwindcss(),
     tanstackStart(),
+    viteTsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
     viteReact(),
   ],
 })
